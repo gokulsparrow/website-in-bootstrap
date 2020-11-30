68e1a6025c71f7b751197ceffa618ec651744035
gokulsparrow <59665728+gokulsparrow@users.noreply.github.com>
2020-11-30 15:13:42 +0530

    css path

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({
 
 
 //app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static('public'));
+app.use(express.static('public/'));
 
 app.post('/name', (request, response, next) => {
     const transporter = nodemailer.createTransport({
